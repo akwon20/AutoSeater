f53402542e362a8efa40b2d379049ea36c836990
akwon20 <austin.kwon2016@gmail.com>
2025-04-14 12:22:28 -0700

    feat: created Assigner class

---
+++ app/src/main/java/autoseater/Assigner.java
@@ -0,0 +1,15 @@
+package autoseater;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.variables.IntVar;
+
+
+public class Assigner {
+
+}
