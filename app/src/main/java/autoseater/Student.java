0ef721a487e7e841a814677d50b5f761b418aa77
akwon20 <austin.kwon2016@gmail.com>
2025-04-13 18:17:05 -0700

    feat: initial commit

---
+++ app/src/main/java/autoseater/Student.java
@@ -0,0 +1,25 @@
+package autoseater;
+
+public class Student {
+
+    private int idNum;
+    private String name;
+
+    public Student() {
+        this.idNum = 0;
+        this.name = "null";
+    }
+
+    public Student(int idNum, String name) {
+        this.idNum = idNum;
+        this.name = name;
+    }
+
+    public int getIdNum() {
+        return this.idNum;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
