3d18775c179f873636fa32eebbfea2ca7038c8db
akwon20 <austin.kwon2016@gmail.com>
2025-04-15 15:43:47 -0700

    fix: removed unused imports

--- app/src/main/java/autoseater/Assigner.java
+++ app/src/main/java/autoseater/Assigner.java
@@ -1,9 +1,7 @@
 package autoseater;
 
 import java.security.SecureRandom;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solver;
