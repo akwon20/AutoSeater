a8bb5fe7a7e71a39dd19f84988a55f15f7c1a8bf
akwon20 <austin.kwon2016@gmail.com>
2025-06-11 16:19:18 -0700

    chore: rewrote package for Spring Boot dependencies

---
+++ autoseat-backend/src/test/java/autoseater/AutoseaterApplicationTests.java
@@ -0,0 +1,13 @@
+package autoseater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoseaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
