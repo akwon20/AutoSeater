5027dcfc26d5708fd505725e0a7669287e91c6ad
akwon20 <austin.kwon2016@gmail.com>
2025-06-11 18:18:57 -0700

    chore: rewrote test

--- autoseat-backend/src/test/java/autoseater/AutoseaterApplicationTests.java
+++ autoseat-backend/src/test/java/autoseater/AutoseaterApplicationTests.java
@@ -1,6 +1,11 @@
 package autoseater;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +14,16 @@ class AutoseaterApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+    public void checkStudentList() {
+        List<Student> testStudentList = new ArrayList<Student>();
+
+        for (int i = 0; i < 3; i++) {
+            testStudentList.add(new Student(i, "Student " + i));
+        }
+
+        Assert.assertEquals(3, testStudentList.size());
+    }
 
 }
