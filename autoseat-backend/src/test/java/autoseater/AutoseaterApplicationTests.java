d44638ac665e00bbc2035c6bb0bb7a58ced194b1
akwon20 <austin.kwon2016@gmail.com>
2025-06-19 17:42:50 -0700

    chore: added failing test case; added debug prints

--- autoseat-backend/src/test/java/autoseater/AutoseaterApplicationTests.java
+++ autoseat-backend/src/test/java/autoseater/AutoseaterApplicationTests.java
@@ -1,5 +1,6 @@
 package autoseater;
 
+import java.beans.Transient;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +23,22 @@ class AutoseaterApplicationTests {
             testStudentList.add(new Student(i, "Student " + i));
         }
 
+        System.out.println(testStudentList.size());
+
         Assertions.assertEquals(3, testStudentList.size());
+    }
+
+    @Test
+    public void checkStudentListFail() {
+        List<Student> testStudentList = new ArrayList<Student>();
+
+        for (int i = 0; i < 3; i++) {
+            testStudentList.add(new Student(i, "Student " + i));
+        }
+
+        System.out.println(testStudentList.size());
+
+        Assertions.assertNotEquals(2, testStudentList.size());
     }
 
     @Test
