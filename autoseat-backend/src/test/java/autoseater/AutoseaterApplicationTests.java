9519585fa46500ea351c9d151965e2d380a809e2
akwon20 <austin.kwon2016@gmail.com>
2025-06-20 13:38:57 -0700

    chore: added test cases for student id and name

--- autoseat-backend/src/test/java/autoseater/AutoseaterApplicationTests.java
+++ autoseat-backend/src/test/java/autoseater/AutoseaterApplicationTests.java
@@ -35,4 +35,76 @@ class AutoseaterApplicationTests {
 
         Assertions.assertNotEquals(2, testStudentList.size());
     }
+
+    @Test
+    public void checkStudentId() {
+        List<Student> testStudentList = new ArrayList<Student>();
+
+        int testId;
+
+        for (int i = 0; i < 3; i++) {
+            testStudentList.add(new Student(i, "Student " + i));
+        }
+
+        testId = AutoseaterApplication.getStudentIdByName(testStudentList, testStudentList.get(2).getName());
+
+        System.out.println(testId);
+
+        Assertions.assertNotNull(testId);
+        Assertions.assertEquals(2, testId);
+    }
+
+    @Test
+    public void checkStudentIdFail() {
+        List<Student> testStudentList = new ArrayList<Student>();
+
+        int testId;
+
+        for (int i = 0; i < 3; i++) {
+            testStudentList.add(new Student(i, "Student " + i));
+        }
+
+        testId = AutoseaterApplication.getStudentIdByName(testStudentList, testStudentList.get(0).getName());
+
+        System.out.println(testId);
+
+        Assertions.assertNotNull(testId);
+        Assertions.assertNotEquals(2, testId);
+    }
+
+    @Test
+    public void checkStudentName() {
+        List<Student> testStudentList = new ArrayList<Student>();
+
+        String testName;
+
+        for (int i = 0; i < 3; i++) {
+            testStudentList.add(new Student(i, "Student " + i));
+        }
+
+        testName = AutoseaterApplication.getStudentNameById(testStudentList, testStudentList.get(2).getIdNum());
+
+        System.out.println(testName);
+
+        Assertions.assertNotNull(testName);
+        Assertions.assertEquals("Student 2", testName);
+    }
+
+    @Test
+    public void checkStudentNameFail() {
+        List<Student> testStudentList = new ArrayList<Student>();
+
+        String testName;
+
+        for (int i = 0; i < 3; i++) {
+            testStudentList.add(new Student(i, "Student " + i));
+        }
+
+        testName = AutoseaterApplication.getStudentNameById(testStudentList, testStudentList.get(0).getIdNum());
+
+        System.out.println(testName);
+
+        Assertions.assertNotNull(testName);
+        Assertions.assertNotEquals("Student 2", testName);
+    }
 }
