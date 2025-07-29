665dd71327a77f40600e53d900c81f64f9359cd3
akwon20 <austin.kwon2016@gmail.com>
2025-07-28 17:51:54 -0700

    chore: removed unused import

--- autoseat-frontend/src/components/ConstraintRow.js
+++ autoseat-frontend/src/components/ConstraintRow.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 import Button from 'react-bootstrap/button';
 import Form from 'react-bootstrap/Form';
