a7fa55243fb13754bf62226b8f31ca77bea2c1e2
akwon20 <austin.kwon2016@gmail.com>
2025-07-28 17:51:38 -0700

    chore: removed dead code

--- autoseat-frontend/src/components/ConstraintRow.js
+++ autoseat-frontend/src/components/ConstraintRow.js
@@ -5,11 +5,6 @@ import Form from 'react-bootstrap/Form';
 import './Tab.css';
 
 const ConstraintRow = (props) => {
-    // const [val1, setVal1] = useState('');
-    // const [val2, setVal2] = useState('');
-    // const [condition, setCondition] = useState('');
-    // const [constraint, setConstraint] = useState('');
-
     const [constraintInput, setConstraintInput] = useState(['', '', '']);
 
     const constraintChangeHandler = props.constraintChangeHandler;
@@ -22,29 +17,13 @@ const ConstraintRow = (props) => {
     const key = props.key;
     const data = props.data;
 
-    // useEffect(() => {
-    //     console.log("Constraint: ", constraintInput);
-    // }, [constraintInput]);
-
     // 0 = val1, 1 = condition, 2 = val2
     const updateConstraintValue = (index, newInput) => {
         console.log("updateConstraintValue() called!");
         console.log("Updating index ", index, " with ", newInput);
-        // const newConstraintInput = constraintInput.map((constraint, i) => {
-        //     if (i === index) {
-        //         return newInput;
-        //     }
-        //     else {
-        //         return constraint;
-        //     }
-        // });
-
-        // console.log(newConstraintInput);
-
-        // setConstraintInput(newConstraintInput);
 
         setConstraintInput(
-            prevInputs =>
+            (prevInputs) =>
                 prevInputs.map((input, i) => (i === index ? newInput : input))
         );
         console.log("Constraint input updated!");
@@ -56,9 +35,6 @@ const ConstraintRow = (props) => {
         <div key={key} className="d-flex mb-3 justify-content-between">
             <Form.Select className="select-dim-custom" size="sm"
             onChange={(e) => {
-                // setVal1(e.target.value);
-                // setConstraint((prevConstraint) => prevConstraint = val1 + ' ' + condition + ' ' + val2);
-                // constraintRef.current = constraint;
                 updateConstraintValue(0, e.target.value);
             }}>
                 {
@@ -67,9 +43,6 @@ const ConstraintRow = (props) => {
             </Form.Select>
             <Form.Select className="select-middle-dim-custom" size="sm"
             onChange={(e) => {
-                // setCondition(e.target.value);
-                // setConstraint((prevConstraint) => prevConstraint = val1 + ' ' + condition + ' ' + val2);
-                // constraintRef.current = constraint;
                 updateConstraintValue(1, e.target.value);
             }}>
                 {
@@ -78,9 +51,6 @@ const ConstraintRow = (props) => {
             </Form.Select>
             <Form.Select className="select-dim-custom" size="sm"
             onChange={(e) => {
-                // setVal2(e.target.value);
-                // setConstraint((prevConstraint) => prevConstraint = val1 + ' ' + condition + ' ' + val2);
-                // constraintRef.current = constraint;
                 updateConstraintValue(2, e.target.value);
             }}>
                 {
