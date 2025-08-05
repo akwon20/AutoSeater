59b8e49949f08585b6cc4dc3d4fb49f2cd30e6e6
akwon20 <austin.kwon2016@gmail.com>
2025-08-05 14:10:55 -0700

    fix: resolved index typos in left/right constraints

--- autoseat-backend/src/main/java/autoseater/Assigner.java
+++ autoseat-backend/src/main/java/autoseater/Assigner.java
@@ -60,11 +60,9 @@ public class Assigner {
                         model.or(
                                 model.arithm(seat[r][k], "!=", val_forbidden1),
                                 model.arithm(seat[r][k-1], "!=", val_forbidden2)).post();
-                        if (rows > 1) {
-                            model.or(
-                                    model.arithm(seat[r][k], "!=", val_forbidden2),
-                                    model.arithm(seat[1][k-1], "!=", val_forbidden1)).post();
-                        }
+                        model.or(
+                                model.arithm(seat[r][k], "!=", val_forbidden2),
+                                model.arithm(seat[r][k-1], "!=", val_forbidden1)).post();
                     }
 
                     if (k < cols - 1) { // Right
@@ -72,11 +70,9 @@ public class Assigner {
                         model.or(
                                 model.arithm(seat[r][k], "!=", val_forbidden1),
                                 model.arithm(seat[r][k+1], "!=", val_forbidden2)).post();
-                        if (rows > 1) {
-                            model.or(
-                                    model.arithm(seat[r][k], "!=", val_forbidden2),
-                                    model.arithm(seat[1][k+1], "!=", val_forbidden1)).post();
-                        }
+                        model.or(
+                                model.arithm(seat[r][k], "!=", val_forbidden2),
+                                model.arithm(seat[r][k+1], "!=", val_forbidden1)).post();
                     }
                 }
             }
