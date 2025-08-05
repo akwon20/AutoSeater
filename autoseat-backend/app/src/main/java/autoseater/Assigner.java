47108d80e50e69c557e5f57dc59cd10e4d67fcc9
akwon20 <austin.kwon2016@gmail.com>
2025-08-05 11:12:37 -0700

    fix: set row indices to r to address out of bounds exception

--- autoseat-backend/app/src/main/java/autoseater/Assigner.java
+++ autoseat-backend/app/src/main/java/autoseater/Assigner.java
@@ -52,11 +52,9 @@ public class Assigner {
                         model.or(
                                 model.arithm(seat[r][k], "!=", val_forbidden1),
                                 model.arithm(seat[r][k-1], "!=", val_forbidden2)).post();
-                        if (rows > 1) {
-                            model.or(
-                                    model.arithm(seat[r][k], "!=", val_forbidden2),
-                                    model.arithm(seat[1][k-1], "!=", val_forbidden1)).post();
-                        }
+                        model.or(
+                                model.arithm(seat[r][k], "!=", val_forbidden2),
+                                model.arithm(seat[r][k-1], "!=", val_forbidden1)).post();
                     }
 
                     if (k < cols - 1) { // Right
@@ -64,11 +62,9 @@ public class Assigner {
                         model.or(
                                 model.arithm(seat[r][k], "!=", val_forbidden1),
                                 model.arithm(seat[r][k+1], "!=", val_forbidden2)).post();
-                        if (rows > 1) {
-                            model.or(
-                                    model.arithm(seat[r][k], "!=", val_forbidden2),
-                                    model.arithm(seat[1][k+1], "!=", val_forbidden1)).post();
-                        }
+                        model.or(
+                                model.arithm(seat[r][k], "!=", val_forbidden2),
+                                model.arithm(seat[r][k+1], "!=", val_forbidden1)).post();
                     }
                 }
             }
